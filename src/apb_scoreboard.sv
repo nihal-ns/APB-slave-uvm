596822d50d9f092334cf29901caf7c8883cf9163
nihal-ns <nihalns2002@gmail.com>
2025-11-30 11:58:28 +0530

    added the scoreboard logic

--- src/apb_scoreboard.sv
+++ src/apb_scoreboard.sv
@@ -11,6 +11,9 @@ class apb_scoreboard extends uvm_scoreboard;
 	uvm_analysis_imp_mon_pass #(apb_slave-seq_item, apb_scoreboard) item_pass_port;
 
 	virtual apb_slave_intf vif;
+	apb_slave_seq_item act_packet_q[$];
+	apb_slave_seq_item pass_packet_q[$];
+	bit [`WIDTH-1:0] mem[*];
 
 	function new(string name = "apb_scoreboard", uvm_component parent);
 		super.new(name,parent);
@@ -25,17 +28,50 @@ class apb_scoreboard extends uvm_scoreboard;
 
 	virtual function void write_mon_act(seq_item pkt);
 		`uvm_info(get_type_name(), "Received input packet ", UVM_DEBUG)
-		/* mon_act_packet_q.push_back(pkt); */
+		act_packet_q.push_back(pkt);
 	endfunction: write_mon_act
 
 	virtual function void write_mon_pass(seq_item pkt);
 		`uvm_info(get_type_name(), "Received output packet ", UVM_DEBUG)
-		/* mon_pass_packet_q.push_back(pkt); */
+		pass_packet_q.push_back(pkt);
 	endfunction: write_mon_pass
 
 	virtual task run_phase(uvm_phase phase);
+		apb_slave_seq_item act_pkt, pass_pkt;
     forever begin
+			fork
 
+				//write transaction
+				begin
+					wait(act_packet_q.size() > 0);
+					act_pkt = act_packet_q.pop_front();
+
+					if (act_pkt.PWRITE == 1 && !pass_pkt.PSLVERR) begin
+						mem[act_pkt.PADDR] = act_pkt.PWDATA;
+            `uvm_info(get_type_name(), $sformatf("Wrote to address %0d with data %0d\n", act_pkt.PADDR, act_pkt.PWDATA), UVM_MEDIUM)
+					end
+				end
+				
+				// read transaction
+				begin
+					wait(pass_packet_q.size() > 0); 
+          pass_pkt = pass_packet_q.pop_front();
+
+					if(!pass_pkt.PSLVERR) begin
+						if (pass_pkt.PRDATA == mem[act_pkt.PADDR]) begin
+							`uvm_info(get_type_name(), $sformatf("SCOREBOARD PASSED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA), UVM_LOW)
+						end
+						else begin
+							`uvm_error(get_type_name(), $sformatf("SCOREBOARD FAILED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA))
+						end
+					end
+					else
+						$display("Slave error detected");
+				end
+				$display("-----------------------------------------------------------------------------");
+				end
+
+			join
 		end
 	endtask: run_phase
 
