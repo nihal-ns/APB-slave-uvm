77174beba22a1649f94e2f188b83c890e0fa07ef
nihal-ns <nihalns2002@gmail.com>
2025-12-02 13:00:52 +0530

    added three versions of logic

--- src/apb_scoreboard.sv
+++ src/apb_scoreboard.sv
@@ -14,6 +14,7 @@ class apb_scoreboard extends uvm_scoreboard;
 	apb_slave_seq_item act_packet_q[$];
 	apb_slave_seq_item pass_packet_q[$];
 	bit [`WIDTH-1:0] mem[*];
+	int MATCH, MISMATCH;
 
 	function new(string name = "apb_scoreboard", uvm_component parent);
 		super.new(name,parent);
@@ -40,39 +41,167 @@ class apb_scoreboard extends uvm_scoreboard;
 		apb_slave_seq_item act_pkt, pass_pkt;
     forever begin
 			fork
-
-				//write transaction
-				begin : write_start
+				begin
 					wait(act_packet_q.size() > 0);
-					act_pkt = act_packet_q.pop_front();
+					act_pkt  = act_packet_q.pop_front();
+				end
 
-					if (act_pkt.PWRITE == 1 && !pass_pkt.PSLVERR) begin : if_write
-						mem[act_pkt.PADDR] = act_pkt.PWDATA;
-            `uvm_info(get_type_name(), $sformatf("Wrote to address %0d with data %0d\n", act_pkt.PADDR, act_pkt.PWDATA), UVM_MEDIUM)
-					end : if_write
-				end : write_start
-				
-				// read transaction
-				begin : read_start
-					wait(pass_packet_q.size() > 0); 
-          pass_pkt = pass_packet_q.pop_front();
-
-					if(!pass_pkt.PSLVERR) begin : if_error
-						if (pass_pkt.PRDATA == mem[act_pkt.PADDR]) begin : if_read
-							`uvm_info(get_type_name(), $sformatf("SCOREBOARD PASSED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA), UVM_LOW)
-						end : if_read
-						else begin : else_read
-							`uvm_error(get_type_name(), $sformatf("SCOREBOARD FAILED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA))
-						end : else_read
-					end : if_error
-					else 
-						$display("Slave error detected");
-				$display("-----------------------------------------------------------------------------");
-				end : read_start
-
+				begin
+					wait(pass_packet_q.size() > 0);
+					pass_pkt = pass_packet_q.pop_front();
+				end
 			join
+			compare(act_pkt,pass_pkt);
+			$display("\n==========================================================================================================\n");
 		end
 	endtask: run_phase
+
+	
+	virtual task compare(apb_slave_seq_item act_pkt, pass_pkt);
+		
+		if(act_pkt.PWRITE) begin : write_transaction
+
+			////////////////////////////////////////////
+			//			Write transaction part            //
+			////////////////////////////////////////////
+			if(!pass_pkt.PSLVERR) begin : write_pass
+			
+				// strobe part
+				/* for(int i=0; i<4; i++) begin */
+				/* 	if(act_pkt.PSTRB[i]) begin */
+				/* 		mem[act_pkt.PADDR][(i*8) +: 8] = act_pkt.PWDATA[(i*8) +: 8]; */
+				/* 	end */
+				/* end */
+
+				mem[act_pkt.PADDR] = act_pkt.PWDATA;
+
+				/* `uvm_info(get_type_name(), $sformatf("WRITE SUCCESS: Addr:%0d Data:%0d Strobe:%b", act_pkt.PADDR, mem[act_pkt.PADDR], act_pkt.PSTRB), UVM_MEDIUM) */
+				`uvm_info(get_type_name(), $sformatf("WRITE SUCCESS: Addr:%0d Data:%0d ", act_pkt.PADDR, mem[act_pkt.PADDR]), UVM_MEDIUM)
+				end : write_pass
+				else begin : write_fail
+					`uvm_info(get_type_name(), $sformatf("WRITE ERROR (PSLVERR): Addr:%0d", act_pkt.PADDR), UVM_MEDIUM)
+				end : write_fail
+			
+		end : write_transaction
+
+		////////////////////////////////////////////
+		//				read transaction part           //
+		////////////////////////////////////////////
+		else begin : read_transaction
+			if (!pass_pkt.PSLVERR) begin : read_pass
+
+				if (pass_pkt.PRDATA == mem[act_pkt.PADDR]) begin : read_data_pass
+					`uvm_info(get_type_name(), $sformatf("READ PASSED: Addr:%0d Expected:%0d Got:%0d", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA), UVM_LOW)
+					MATCH++;
+				end : read_data_pass
+				else begin : read_data_fail
+					`uvm_error(get_type_name(), $sformatf("READ FAILED: Addr:%0d Expected:%0d Got:%0d", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA))
+					MISMATCH++;
+				end : read_data_fail
+			end : read_pass
+			else begin : read_fail
+				`uvm_info(get_type_name(), $sformatf("READ ERROR (PSLVERR): Addr:%0d", act_pkt.PADDR), UVM_MEDIUM)
+			end : read_fail
+
+		end : read_transaction
+	
+	endtask: compare
+
+	// version one ==============================================================================================//
+	//
+	/* virtual task run_phase(uvm_phase phase); */
+	/* 	apb_slave_seq_item act_pkt, pass_pkt; */
+	/* 	logic [`WIDTH-1:0] expected_data; */
+
+	/* 	forever begin */
+	/* 		wait(act_packet_q.size() > 0 && pass_packet_q.size() > 0); */
+
+	/* 		act_pkt  = act_packet_q.pop_front(); */
+	/* 		pass_pkt = pass_packet_q.pop_front(); */
+
+	/* 		if (act_pkt.PWRITE) begin */
+	/* 			// --- WRITE TRANSACTION --- */
+	/* 			if (!pass_pkt.PSLVERR) begin */
+					
+	/* 				for(int i=0; i<4; i++) begin */
+	/* 					if(act_pkt.PSTRB[i]) begin */
+	/* 						mem[act_pkt.PADDR][(i * (`WIDTH/4)) +: (`WIDTH/4)] = act_pkt.PWDATA[(i * (`WIDTH/4)) +: (`WIDTH/4)]; */
+	/* 					end */
+	/* 				end */
+
+	/* 				`uvm_info(get_type_name(), $sformatf("WRITE SUCCESS: Addr:0x%0h Data:0x%0h Strobe:%b", act_pkt.PADDR, mem[act_pkt.PADDR], act_pkt.PSTRB), UVM_MEDIUM) */
+	/* 			end */ 
+	/* 			else begin */
+	/* 				`uvm_info(get_type_name(), $sformatf("WRITE ERROR (PSLVERR): Addr:0x%0h", act_pkt.PADDR), UVM_MEDIUM) */
+	/* 			end */
+	/* 		end */ 
+	/* 		else begin */
+	/* 			// --- READ TRANSACTION --- */
+	/* 			if (!pass_pkt.PSLVERR) begin */
+
+	/* 				if (pass_pkt.PRDATA == expected_data) begin */
+	/* 					`uvm_info(get_type_name(), $sformatf("READ PASSED: Addr:0x%0h Expected:0x%0h Got:0x%0h", act_pkt.PADDR, expected_data, pass_pkt.PRDATA), UVM_LOW) */
+	/* 				end */ 
+	/* 				else begin */
+	/* 					`uvm_error(get_type_name(), $sformatf("READ FAILED: Addr:0x%0h Expected:0x%0h Got:0x%0h", act_pkt.PADDR, expected_data, pass_pkt.PRDATA)) */
+	/* 				end */
+	/* 			end */ 
+	/* 			else begin */
+	/* 				`uvm_info(get_type_name(), $sformatf("READ ERROR (PSLVERR): Addr:0x%0h", act_pkt.PADDR), UVM_MEDIUM) */
+	/* 			end */
+	/* 		end */
+			
+	/* 		$display("-----------------------------------------------------------------------------"); */
+	/* 	end */
+	/* endtask: run_phase */
+
+	// version two ==============================================================================================//
+/* 		virtual task run_phase(uvm_phase phase); */
+/* 		apb_slave_seq_item act_pkt, pass_pkt; */
+/*     forever begin */
+/* 			fork */
+
+/* 				//write transaction */
+/* 				begin : write_start */
+/* 					wait(act_packet_q.size() > 0); */
+/* 					act_pkt = act_packet_q.pop_front(); */
+
+/* 					if (act_pkt.PWRITE == 1 && !pass_pkt.PSLVERR) begin : if_write */
+/* 						mem[act_pkt.PADDR] = act_pkt.PWDATA; */
+/*             `uvm_info(get_type_name(), $sformatf("Wrote to address %0d with data %0d\n", act_pkt.PADDR, act_pkt.PWDATA), UVM_MEDIUM) */
+/* 					end : if_write */
+/* 				end : write_start */
+
+/* 				// read transaction */
+/* 				begin : read_start */
+/* 					wait(pass_packet_q.size() > 0); */
+/*           pass_pkt = pass_packet_q.pop_front(); */
+
+/* 					if(!pass_pkt.PSLVERR) begin : if_error */
+/* 						if (pass_pkt.PRDATA == mem[act_pkt.PADDR]) begin : if_read */
+/* 							`uvm_info(get_type_name(), $sformatf("SCOREBOARD PASSED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA), UVM_LOW) */
+/* 						end : if_read */
+/* 						else begin : else_read */
+/* 							`uvm_error(get_type_name(), $sformatf("SCOREBOARD FAILED: Addr: %0d, Expected: %0d, Got: %0d\n", act_pkt.PADDR, mem[act_pkt.PADDR], pass_pkt.PRDATA)) */
+/* 						end : else_read */
+/* 					end : if_error */
+/* 					else */
+/* 						$display("Slave error detected"); */
+/* 				$display("-----------------------------------------------------------------------------"); */
+/* 				end : read_start */
+
+/* 			join */
+/* 		end */
+/* 	endtask: run_phase */
+
+
+  function void report_phase(uvm_phase phase);
+    super.report_phase(phase);
+		$display("\n==========================================================================================================\n");
+    `uvm_info("SCB", $sformatf("||| TOTAL MATCHES     : %0d |||", MATCH), UVM_NONE)
+    `uvm_info("SCB", $sformatf("||| TOTAL MISSMATCHES : %0d |||", MISMATCH), UVM_NONE)
+		$display("\n==========================================================================================================\n");
+  endfunction
 
 endclass: apb_scoreboard
 
